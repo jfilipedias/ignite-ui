11a1a0dbf3d9843adc85b7ec534579f25fb41737
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-02-14 15:08:45 -0300

    Creates the TextArea component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,35 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Comment</Text>
+          {story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+type Story = StoryObj<TextAreaProps>
+
+export const Primary: Story = {
+  args: {
+    placeholder: 'Add your comment',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from 'react'
+import { styled } from '@/styles'
+
+export const TextArea = styled('textarea', {
+  boxSizing: 'border-box',
+  minHeight: 80,
+  padding: '$3 $4',
+  border: '2px solid $gray900',
+  borderRadius: '$sm',
+  backgroundColor: '$gray900',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  color: '$white',
+  resize: 'vertical',
+
+  '&:focus': {
+    outline: 0,
+    borderColor: '$ignite300',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+})
+
+export type TextAreaProps = ComponentProps<typeof TextArea>
