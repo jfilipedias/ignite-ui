f0dfcbb8009d0a8070862f9f2272b94ec9a8a09e
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-02-14 08:34:29 -0300

    Creates the avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar ',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/jfilipedias.png',
+    alt: 'Filipe Dias',
+  },
+} as Meta<AvatarProps>
+
+type Story = StoryObj<AvatarProps>
+
+export const Primary: Story = {}
+
+export const WithFallback: Story = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/react/src/components/Avatar.tsx
@@ -0,0 +1,48 @@
+import { User } from 'phosphor-react'
+import * as AvatarPrimitive from '@radix-ui/react-avatar'
+import { styled } from '@/styles'
+
+const AvatarRoot = styled(AvatarPrimitive.Root, {
+  display: 'inline-block',
+  width: '$12',
+  height: '$12',
+  borderRadius: '$full',
+  overflow: 'hidden',
+})
+
+const AvatarImage = styled(AvatarPrimitive.Image, {
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  borderRadius: 'inherit',
+})
+
+const AvatarFallback = styled(AvatarPrimitive.Fallback, {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100%',
+  backgroundColor: '$gray600',
+  color: '$gray800',
+
+  svg: {
+    width: '$6',
+    height: '$6',
+    color: '$gray400',
+  },
+})
+
+export type AvatarProps = AvatarPrimitive.AvatarImageProps
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarRoot>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarRoot>
+  )
+}
