7e9fee2c14e8c6e0d23e50b30f24f36ae14bbdcd
Filipe Dias <filipediascontato@gmail.com>
2023-02-24 18:51:36 -0300

    Adds the storybook a11y plugin

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     "../src/stories/**/*.stories.tsx"
   ],
   "addons": [
+    "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
