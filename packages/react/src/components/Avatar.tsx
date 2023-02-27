121fdce24b6d3a570b4186c0fe5333b81ea09618
Filipe Dias <filipediascontato@gmail.com>
2023-02-26 21:10:22 -0300

    fix: Updates the Avatar size

--- packages/react/src/components/Avatar.tsx
+++ packages/react/src/components/Avatar.tsx
@@ -4,8 +4,8 @@ import { styled } from '@/styles'
 
 const AvatarRoot = styled(AvatarPrimitive.Root, {
   display: 'inline-block',
-  width: '$12',
-  height: '$12',
+  width: '$16',
+  height: '$16',
   borderRadius: '$full',
   overflow: 'hidden',
 })
